dbe65ff4ea62665b520bf9e0d0192955ed3c8a36
ganevv <ganeww@mail.bg>
2022-12-12 17:47:21 +0200

    update login and register components

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service';
+import { FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -7,4 +10,13 @@ import { Component } from '@angular/core';
 })
 export class RegisterComponent {
 
+  validationPattern = '^[a-zA-Z0-9]{3,}@[a-z]+\.[a-z]+$'
+
+  constructor(private router: Router, private authService: AuthService, private formBuilder: FormBuilder) { }
+
+  errors: string | undefined = undefined
+
+  // register(): void {
+  //   const { email, username, password } = 
+  // }
 }
