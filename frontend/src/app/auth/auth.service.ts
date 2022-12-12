f9d499eac3665d9e5c8b75d83edf1f6b9a48963d
ganevv <ganeww@mail.bg>
2022-12-12 16:07:00 +0200

    login update

--- frontend/src/app/auth/auth.service.ts
+++ frontend/src/app/auth/auth.service.ts
@@ -1,9 +1,44 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+import { tap } from 'rxjs'
+import { environment } from 'src/environments/environment';
+import { IUser } from '../shared/interfaces/user';
+
+const apiURL = environment.apiURL
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  user: IUser | null = null
+
+  constructor(private http: HttpClient, private router: Router) { }
+
+  getUser() {
+    return this.http.get<IUser>(`${apiURL}/auth/user`).pipe(tap((user) => {
+      this.user = user
+    }))
+  }
+
+  register(userData: {}) {
+    return this.http.post<IUser>(`${apiURL}/auth/register`, userData).pipe(tap((user) => {
+      this.user = user
+      localStorage.setItem('token', this.user.accessToken)
+    }))
+  }
+
+  login(userData: {}) {
+    return this.http.post<IUser>(`${apiURL}/auth/login`, userData).pipe(tap((userData) => {
+      this.user = userData
+      localStorage.setItem('token', this.user.accessToken)
+    }))
+  }
+
+  logout() {
+    this.user = null
+    return localStorage.removeItem('token')
+  }
+
 }
--- frontend/src/app/auth/login/login.component.ts
+++ frontend/src/app/auth/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from '../auth.service'
 
 @Component({
   selector: 'app-login',
@@ -7,4 +10,28 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  validationPattern = '^[a-zA-Z0-9]{3,}@[a-z]+\.[a-z]+$'
+
+  loginGroup: FormGroup = this.formBuilder.group({
+    'email': new FormControl('', [Validators.required, Validators.pattern(this.validationPattern)]),
+    'password': new FormControl(null, [Validators.required, Validators.minLength(4)])
+  })
+
+  constructor(private router: Router, private authService: AuthService, private formBuilder: FormBuilder) { }
+
+  errors: string | undefined = undefined
+
+  login(): void {
+    const { email, password } = this.loginGroup.value
+    const body = { email, password }
+
+    this.authService.login(body).subscribe({
+      next: () => {
+        this.router.navigate(['/'])
+      },
+      error: (err) => {
+        this.errors = err.error
+      }
+    })
+  }
 }
