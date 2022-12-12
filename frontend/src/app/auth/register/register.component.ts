de67e340c447a0a18bc298027074295d67ed8584
ganevv <ganeww@mail.bg>
2022-12-12 18:19:14 +0200

    minor updates

--- frontend/src/app/auth/register/register.component.ts
+++ frontend/src/app/auth/register/register.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { FormBuilder } from '@angular/forms'
+import { FormBuilder, Validators } from '@angular/forms'
 
 @Component({
   selector: 'app-register',
@@ -12,11 +12,29 @@ export class RegisterComponent {
 
   validationPattern = '^[a-zA-Z0-9]{3,}@[a-z]+\.[a-z]+$'
 
+  form = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.pattern(this.validationPattern)]],
+    username: ['', [Validators.required, Validators.minLength(5)]],
+    password: ['', [Validators.required, Validators.minLength(5)]],
+    rePassword: []
+    //todo pass and repass must be same
+  })
+
   constructor(private router: Router, private authService: AuthService, private formBuilder: FormBuilder) { }
 
   errors: string | undefined = undefined
 
-  // register(): void {
-  //   const { email, username, password } = 
-  // }
+  register(): void {
+    if (this.form.invalid) { return }
+    const { email, username, password, rePassword } = this.form.value
+    this.authService.register({ email, username, password, rePassword })
+      .subscribe({
+        next: () => {
+          this.router.navigate(['/'])
+        },
+        error: (err) => {
+          this.errors = err.error
+        }
+      })
+  }
 }
