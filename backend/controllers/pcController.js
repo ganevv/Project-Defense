06b582516ac30223b9d06b251eba2d3e2a17aee4
ganevv <ganeww@mail.bg>
2022-12-12 11:03:29 +0200

    database updated

--- backend/controllers/pcController.js
+++ backend/controllers/pcController.js
@@ -1,5 +1,5 @@
 const { hasUser } = require('../middlewares/guards')
-const { getAll, create, getById } = require('../services/pc')
+const { getAll, create, getById, update, deleteById } = require('../services/pc')
 
 const pcController = require('express').Router()
 
@@ -20,6 +20,31 @@ pcController.post('/', hasUser(), async (req, res) => {
 
 pcController.get('/:id', async (req, res, next) => {
     res.json(await getById(req.params.id))
+})
+
+pcController.put('/:id', hasUser(), async (req, res, next) => {
+    const pc = await getById(req.params.id)
+    if (req.user._id != pc._ownerId) {
+        return res.status(403).json({ message: 'You cannot modify this Pc' })
+    }
+    try {
+        res.json(await update(req.params.id, req.body))
+    } catch (error) {
+        res.status(400).json({ error: err.message })
+    }
+})
+
+pcController.delete('/:id', hasUser(), async (req, res) => {
+    const pc = await getById(req.params.id)
+    if (req.user._id != pc._ownerId) {
+        return res.status(403).json({ message: 'You cannot modify this Pc' })
+    }
+    try {
+        await deleteById(req.params.id)
+        res.status(204).end()
+    } catch (error) {
+        res.status(400).json({ error: err.message })
+    }
 })
 
 module.exports = {
